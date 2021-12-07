60893d1dfdfb6991f294bf722356dc54bb6379c3
Leo Niemi <niemi.leo@gmail.com>
2021-12-07 11:00:50 +0200

    Refactor application to better structure

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
-const http = require('http')
-const express = require('express')
+const config = require('./utils/config')
+const express =require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const blog = require('./models/blog')
+const { builtinModules } = require('module')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+logger.info('Connecting to: ', config.MONGODB_URI)
 
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
