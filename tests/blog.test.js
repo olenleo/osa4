027b9c7680e0b442c3a5d01edac07460e77f193c
Leo Niemi <niemi.leo@gmail.com>
2021-12-25 21:28:09 +0200

    4.5 favouriteBlog function with tests

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -7,7 +7,6 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
-
 describe('total likes', () => {
     const emptyList = []
     const listOfOne = [
@@ -69,7 +68,7 @@ describe('total likes', () => {
           likes: 2,
           __v: 0
         }  
-      ]
+    ]
       
     test('of empty bloglist is 0', () => {
         const result = listHelper.totalLikes(emptyList)
@@ -80,10 +79,92 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(listOfOne)
         expect(result).toBe(7)
       })
-            
-      
+               
       test('of a bigger list are correct', () => {
         const result = listHelper.totalLikes(longList)
         expect(result).toBe(36)
       })
+
   })
+describe('favourite blog search', () => {
+    const listOfOne = [
+        {
+            _id: "5a422a851b54a676234d17f7",
+            title: "React patterns",
+            author: "Michael Chan",
+            url: "https://reactpatterns.com/",
+            likes: 7,
+            __v: 0
+        }
+    ]
+    const longList = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          author: "Michael Chan",
+          url: "https://reactpatterns.com/",
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+          likes: 12,
+          __v: 0
+        },
+        {
+          _id: "5a422b891b54a676234d17fa",
+          title: "First class tests",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+          likes: 10,
+          __v: 0
+        },
+        {
+          _id: "5a422ba71b54a676234d17fb",
+          title: "TDD harms architecture",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+          likes: 0,
+          __v: 0
+        },
+        {
+          _id: "5a422bc61b54a676234d17fc",
+          title: "Type wars",
+          author: "Robert C. Martin",
+          url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+          likes: 2,
+          __v: 0
+        }  
+    ]
+    test('of listOfOne returns correct', () => {
+        const result = listHelper.favouriteBlog(listOfOne)
+        testObject =  {
+            title: "React patterns",
+            author: "Michael Chan",
+            likes: 7,
+        }
+        expect(result).toEqual(testObject)
+    })
+    test('of longList returns correct', () => {
+        const result = listHelper.favouriteBlog(longList)
+        testObject = {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12,
+          }
+        expect(result).toEqual(testObject)
+
+    })
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,9 +10,26 @@ const totalLikes = ( blogs ) => {
         likes += blog.likes
     });
     return likes
+}
 
+const favouriteBlog = ( blogs ) => {
+    let bestBlog
+    let max = 0
+    blogs.forEach(blog => {
+        if (blog.likes > max) {
+            bestBlog = blog
+             max = bestBlog.likes
+            }
+    })
+    toReturn = {
+        title: bestBlog.title,
+        author: bestBlog.author,
+        likes: bestBlog.likes
+    }
+    return toReturn
 }
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
   }
